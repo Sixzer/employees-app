0c6dd5aa56f90d66cffec2c5a263f7805efbfb6f
Sixzer <iam6zer@gmail.com>
2022-09-14 12:29:28 +0300

    add cookie

--- src/components/emp-list-item/emp-list-item.js
+++ src/components/emp-list-item/emp-list-item.js
@@ -1,33 +1,65 @@
+import { Component } from 'react';
 import "./emp-list-item.css";
 
-const EmpListItem = function({name, salary, cookie}) {
+class EmpListItem extends Component {
 
-    let classNames = "list-group-item d-flex justify-content-between"
-
-    if (cookie === true){
-        classNames += " increase"
+    constructor(props) {
+        super(props);
+        this.state = {
+            cookie: false,
+            liked: false,
+        }
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={`${salary} $`} />
-            <div className="d-flex justify-content-center align-items-center">
-                <button
-                    type="button"
-                    className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onCookie = () => {
+        this.setState(({cookie}) => ({
+            cookie: !cookie,
+        }))
+    }
 
-                <button
-                    type="button"
-                    className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
+    isLiked = () => {
+        this.setState(({liked})=> ({
+            liked: !liked,
+        }))
+    }
+
+    render() {
+
+        const { name, salary } = this.props;
+        const { cookie, liked } = this.state;
+        let classNames = "list-group-item d-flex justify-content-between"
+
+        if (cookie === true) {
+            classNames += " increase"
+        }
+        if (liked === true) {
+            classNames += " like"
+        }
+    
+        return (
+            <li className={classNames}>
+                <span 
+                    className="list-group-item-label"
+                    onClick={this.isLiked}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={`${salary} $`} />
+                <div className="d-flex justify-content-center align-items-center">
+                    <button 
+                        type="button"
+                        className="btn-cookie btn-sm"
+                        onClick={this.onCookie}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button
+                        type="button"
+                        className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
 };
 
 export default EmpListItem;
